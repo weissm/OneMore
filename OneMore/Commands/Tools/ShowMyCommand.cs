f053f4540467ca34b8f59ccb5925b0b18a8a9ee8
Matthias Weiss <matthias.h.weiss@arcor.de>
2021-10-28 14:06:30 +0200

    added new mom generator file

---
+++ OneMore/Commands/Tools/ShowMyCommand.cs
@@ -0,0 +1,27 @@
+﻿//************************************************************************************************
+// Copyright © 2016 Steven M Cohn.  All rights reserved.
+//************************************************************************************************
+
+namespace River.OneMoreAddIn.Commands
+{
+	using System.Threading.Tasks;
+	using OneNote2X.Forms;
+
+	internal class ShowMyCommand : Command
+	{
+		public ShowMyCommand ()
+		{
+		}
+
+
+		public override async Task Execute(params object[] args)
+		{
+			using (var dialog = new GenMoMs())
+			{
+				dialog.ShowDialog(owner);
+			}
+
+			await Task.Yield();
+		}
+	}
+}
