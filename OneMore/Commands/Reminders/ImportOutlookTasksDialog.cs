492848a61dd7931024bab8d41830f9c1f3d2feca
Matthias Weiss <matthias.h.weiss@arcor.de>
2021-12-27 09:21:29 +0100

    merged 12.1

---
+++ OneMore/Commands/Reminders/ImportOutlookTasksDialog.cs
@@ -0,0 +1,334 @@
+﻿//************************************************************************************************
+// Copyright © 2021 Steven M Cohn.  All rights reserved.
+//************************************************************************************************
+
+#pragma warning disable S3267 // Loops should be simplified with "LINQ" expressions
+
+namespace River.OneMoreAddIn.Commands
+{
+	using Aga.Controls.Tree;
+	using Aga.Controls.Tree.NodeControls;
+	using River.OneMoreAddIn.Helpers.Office;
+	using River.OneMoreAddIn.UI;
+	using System.Collections.Generic;
+	using System.Drawing;
+	using System.Linq;
+	using System.Threading;
+	using System.Windows.Forms;
+	using static River.OneMoreAddIn.OneNote;
+	using Resx = River.OneMoreAddIn.Properties.Resources;
+
+
+	internal partial class ImportOutlookTasksDialog : UI.LocalizableForm
+	{
+		private TreeModel model;
+		private OneNote one;
+		private Outlook outlook;
+
+
+		#region class TaskNodeIcon
+		private sealed class TreeNodeIcon : NodeIcon
+		{
+			private readonly Image task;
+			private readonly Image opened;
+			private readonly Image closed;
+
+			public TreeNodeIcon()
+			{
+				task = Resx.Task;
+				opened = Resx.FolderOpen;
+				closed = Resx.FolderClose;
+			}
+
+			protected override Image GetIcon(TreeNodeAdv node)
+			{
+				if (node.Tag is Node subnode && subnode.Tag is OutlookTask)
+					return task;
+				else if (node.CanExpand && node.IsExpanded)
+					return opened;
+				else
+					return closed;
+			}
+		}
+		#endregion class TaskNodeIcon
+
+
+		public ImportOutlookTasksDialog()
+		{
+			InitializeComponent();
+
+			if (NeedsLocalizing())
+			{
+				Text = Resx.ImportOutlookTasksDialog_Text;
+
+				Localize(new string[]
+				{
+					"introBox",
+					"warningBox",
+					"listButton",
+					"tableButton",
+					"okButton=word_OK",
+					"cancelButton=word_Cancel"
+				});
+			}
+			else
+			{
+				// customization only for English
+				warningBox.Clear();
+				warningBox.AppendText("Note that OneNote does not bind completely to task that " +
+					"are not in the Outlook Tasks folder. Tasks from sub-folders are shown ");
+				warningBox.AppendFormattedText("in red", Color.Firebrick);
+				warningBox.AppendFormattedText(" to indicate that their status flags will not update " +
+					"automatically after importing.", SystemColors.GrayText);
+			}
+
+			resetInfoLabel.Visible = false;
+			resetInfoLabel.Left = resetLabel.Left;
+
+			// prepare TreeViewAdv...
+
+			var nodeCheckBox = new NodeCheckBox
+			{
+				DataPropertyName = "CheckState",
+				EditEnabled = true,
+				LeftMargin = 0
+			};
+			nodeCheckBox.CheckStateChanged += CheckStateChanged;
+			tree.NodeControls.Add(nodeCheckBox);
+
+			tree.NodeControls.Add(new TreeNodeIcon
+			{
+				LeftMargin = 1,
+				ScaleMode = ImageScaleMode.Clip
+			});
+
+			var treeNodeTextBox = new NodeTextBox
+			{
+				DataPropertyName = "Text",
+				LeftMargin = 3
+			};
+			treeNodeTextBox.DrawText += (sender, args) =>
+			{
+				if (args.Node.Level > 2)
+					args.TextColor = Color.Firebrick;
+			};
+			tree.NodeControls.Add(treeNodeTextBox);
+
+			model = new TreeModel();
+			tree.Model = model;
+		}
+
+
+		public ImportOutlookTasksDialog(OutlookTaskFolders folders)
+			: this()
+		{
+			PopulateTree(folders);
+			tree.ExpandAll();
+		}
+
+
+		private void PopulateTree(OutlookTaskFolders folders, Node parent = null)
+		{
+			foreach (var folder in folders)
+			{
+				var node = new Node(folder.Name)
+				{
+					Tag = folder
+				};
+
+				PopulateTree(folder.Folders, node);
+
+				foreach (var task in folder.Tasks)
+				{
+					var leaf = new Node(task.Subject);
+					leaf.Tag = task;
+
+					if (!string.IsNullOrEmpty(task.OneNoteTaskID))
+					{
+						leaf.IsChecked = true;
+						leaf.IsEnabled = false;
+					}
+
+					node.Nodes.Add(leaf);
+				}
+
+				if (parent == null)
+				{
+					model.Nodes.Add(node);
+				}
+				else
+				{
+					parent.Nodes.Add(node);
+				}
+			}
+		}
+
+
+		public IEnumerable<OutlookTask> SelectedTasks =>
+			GetSelectedTask(model.Root, new List<OutlookTask>());
+
+
+		public bool ShowDetailedTable => tableButton.Checked;
+
+
+		private void CheckStateChanged(object sender, TreePathEventArgs e)
+		{
+			if (tree.NodeControls.FirstOrDefault(c => c is NodeCheckBox) is NodeCheckBox box)
+			{
+				box.CheckStateChanged -= CheckStateChanged;
+
+				var node = model.FindNode(e.Path);
+				if (node != null)
+				{
+					if (node.Tag is OutlookTask task)
+					{
+						CheckFolderState(node.Parent);
+					}
+					else if (node.Tag is OutlookTaskFolder folder)
+					{
+						if (node.CheckState != CheckState.Indeterminate)
+						{
+							node.Nodes
+								.Where(n => n.Tag is OutlookTask t && string.IsNullOrEmpty(t.OneNoteTaskID))
+								.ForEach(n => n.CheckState = node.CheckState);
+						}
+					}
+				}
+
+				box.CheckStateChanged += CheckStateChanged;
+			}
+		}
+
+
+		private void CheckFolderState(Node node)
+		{
+			var userChecked = 0;
+			var userUnchecked = 0;
+			foreach (var child in node.Nodes.Where(n => 
+				n.Tag is OutlookTask task && string.IsNullOrEmpty(task.OneNoteTaskID)))
+			{
+				if (child.IsChecked)
+				{
+					userChecked++;
+				}
+				else
+				{
+					userUnchecked++;
+				}
+			}
+
+			if (userChecked > 0 && userUnchecked == 0)
+			{
+				node.IsChecked = true;
+			}
+			else if (userChecked == 0 && userUnchecked > 0)
+			{
+				node.IsChecked = false;
+			}
+			else
+			{
+				node.CheckState = CheckState.Indeterminate;
+			}
+		}
+
+
+		private IEnumerable<OutlookTask> GetSelectedTask(Node node, List<OutlookTask> tasks)
+		{
+			var t = node.Nodes.Where(n => n.Tag is OutlookTask task && 
+				string.IsNullOrEmpty(task.OneNoteTaskID) && n.IsChecked)
+				.Select(n => n.Tag as OutlookTask);
+
+			if (t.Any())
+			{
+				tasks.AddRange(t);
+			}
+
+			foreach (var child in node.Nodes.Where(n => n.Tag is OutlookTaskFolder))
+			{
+				GetSelectedTask(child, tasks);
+			}
+
+			return tasks;
+		}
+
+
+		private async void ResetOrphanedTasks(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			using (one = new OneNote())
+			{
+				var source = new CancellationTokenSource();
+				var map = await one.BuildHyperlinkMap(Scope.Sections, source.Token);
+
+				var count = 0;
+				using (outlook = new Outlook())
+				{
+					count = ResetOrphanedTasks(map, model.Root);
+				}
+
+				resetLabel.Visible = false;
+				resetInfoLabel.Visible = true;
+
+				resetInfoLabel.Text = count == 0
+					? Resx.ImportOutlookTasksDialog_noorphans
+					: string.Format(Resx.ImportOutlookTasksDialog_reset, count);
+			}
+		}
+
+
+		private int ResetOrphanedTasks(Dictionary<string, HyperlinkInfo> map, Node node)
+		{
+			var count = 0;
+			var taskNodes = node.Nodes
+				.Where(n => n.Tag is OutlookTask task && !string.IsNullOrEmpty(task.OneNoteURL));
+
+			if (taskNodes.Any())
+			{
+				foreach (var taskNode in taskNodes)
+				{
+					var task = taskNode.Tag as OutlookTask;
+
+					var key = one.GetHyperKey(task.OneNoteURL);
+					if (map.ContainsKey(key))
+					{
+						var page = one.GetPage(map[key].PageID, PageDetail.Basic);
+						if (page == null)
+						{
+							ResetTask(taskNode, task);
+							count++;
+						}
+						else
+						{
+							if (!page.Root.Descendants(page.Namespace + "OutlookTask")
+								.Any(e => e.Attribute("guidTask").Value == task.OneNoteTaskID))
+							{
+								ResetTask(taskNode, task);
+								count++;
+							}
+						}
+					}
+				}
+			}
+
+			foreach (var child in node.Nodes.Where(n => n.Tag is OutlookTaskFolder))
+			{
+				count += ResetOrphanedTasks(map, child);
+			}
+
+			return count;
+		}
+
+
+		private void ResetTask(Node node, OutlookTask task)
+		{
+			task.OneNoteTaskID = null;
+			task.OneNoteURL = null;
+			task.OneNotePageID = null;
+			task.OneNoteObjectID = null;
+
+			outlook.SaveTask(task);
+
+			node.IsEnabled = true;
+			node.IsChecked = true;
+		}
+	}
+}
---
+++ OneMore/Helpers/Office/Outlook.cs
@@ -0,0 +1,248 @@
+﻿//************************************************************************************************
+// Copyright © 2021 Steven M Cohn.  All rights reserved.
+//************************************************************************************************
+
+namespace River.OneMoreAddIn.Helpers.Office
+{
+	using Microsoft.Office.Interop.Outlook;
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Globalization;
+	using System.Linq;
+	using System.Runtime.InteropServices;
+
+
+	/// <summary>
+	/// Automated interaction with Outlook
+	/// </summary>
+	internal class Outlook : IDisposable
+	{
+		private Application outlook;
+		private bool disposed;
+
+		private Calendar calendar;
+		private CalendarWeekRule weekRule;
+		private DayOfWeek firstDay;
+
+
+		#region class FolderPath
+		private sealed class FolderPath
+		{
+			public string EntryID { get; set; }
+			public string Path { get; set; }
+		}
+		#endregion class FolderPath
+
+
+
+		/// <summary>
+		/// Initializes a new instance of the Outlook interop application. If Outlook is not
+		/// currently running, it will be started
+		/// </summary>
+		public Outlook()
+		{
+			outlook = new Application();
+		}
+
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				// this automation class will create a process with the -Embedding cmdline
+				// switch but a user-started Outlook will not so look for a user process
+				// and skip disposing so we don't interrupt the user's interactive session
+				if (!Process.GetProcessesByName("OUTLOOK")
+					.Any(p =>
+					{
+						var cmd = p.GetCommandLine();
+						return cmd != null && !cmd.Contains("-Embedding");
+					}))
+				{
+					// unfortunately, the above assumptions are not true; if an embedded instance
+					// is running and Outlook UI is then started, the embedded instance will
+					// take over so we still have an -Embedding processing serving UI!
+					//outlook.Quit();
+					Marshal.ReleaseComObject(outlook);
+					disposed = true;
+				}
+			}
+
+			if (outlook != null)
+			{
+				Marshal.ReleaseComObject(outlook);
+				outlook = null;
+			}
+		}
+
+
+		public void Dispose()
+		{
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+
+
+		/// <summary>
+		/// Builds a hierarchy of task folders populated with their tasks
+		/// </summary>
+		/// <returns></returns>
+		public OutlookTaskFolders GetTaskHierarchy()
+		{
+			var culture = CultureInfo.CurrentUICulture;
+			calendar = culture.Calendar;
+			weekRule = culture.DateTimeFormat.CalendarWeekRule;
+			firstDay = culture.DateTimeFormat.FirstDayOfWeek;
+
+			return GetTaskHierarchy(
+				outlook.Session.GetDefaultFolder(OlDefaultFolders.olFolderTasks) as Folder,
+				new OutlookTaskFolders());
+		}
+
+		private OutlookTaskFolders GetTaskHierarchy(
+			Folder parent, OutlookTaskFolders container, string path = null)
+		{
+			var folder = new OutlookTaskFolder
+			{
+				Name = parent.Name,
+				EntryID = parent.EntryID
+			};
+
+			path = path == null ? parent.Name : $"{path}{OutlookTask.PathDelimeter}{parent.Name}";
+
+			var items = parent.Items;
+			items.IncludeRecurrences = true;
+			foreach (TaskItem item in items)
+			{
+				folder.Tasks.Add(new OutlookTask
+				{
+					Subject = item.Subject,
+					EntryID = item.EntryID,
+					Complete = item.Complete,
+					CreationTime = item.CreationTime,
+					DateCompleted = item.DateCompleted,
+					DueDate = item.DueDate,
+					Importance = (OutlookImportance)item.Importance,
+					PercentComplete = item.PercentComplete,
+					StartDate = item.StartDate,
+					Status = (OutlookTaskStatus)item.Status,
+					FolderPath = path,
+					Year = item.DueDate.Year,
+					WoYear = calendar.GetWeekOfYear(item.DueDate, weekRule, firstDay),
+					OneNoteTaskID = item.UserProperties["OneNoteTaskID"]?.Value as string,
+					OneNoteURL = item.UserProperties["OneNoteURL"]?.Value as string,
+					OneNotePageID = item.UserProperties["OneNotePageID"]?.Value as string,
+					OneNoteObjectID = item.UserProperties["OneNoteObjectID"]?.Value as string
+				});
+
+				Marshal.ReleaseComObject(item);
+			}
+
+			container.Add(folder);
+
+			foreach (Folder child in parent.Folders)
+			{
+				GetTaskHierarchy(child, folder.Folders, path);
+				Marshal.ReleaseComObject(child);
+			}
+
+			Marshal.ReleaseComObject(parent);
+			return container;
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="taskIDs"></param>
+		/// <returns></returns>
+		public OutlookTasks LoadTasksByID(IEnumerable<string> taskIDs)
+		{
+			var tasks = new OutlookTasks();
+
+			var hierarchy = GetTaskHierarchy();
+			if (hierarchy.Any())
+			{
+				tasks.AddRange(Flatten(hierarchy).Where(t => taskIDs.Contains(t.OneNoteTaskID)));
+			}
+
+			return tasks;
+		}
+
+
+		private IEnumerable<OutlookTask> Flatten(OutlookTaskFolders folders)
+		{
+			foreach (var folder in folders)
+			{
+				foreach (var task in folder.Tasks
+					.Where(t => !string.IsNullOrEmpty(t.OneNoteTaskID)))
+				{
+					yield return task;
+				}
+
+				foreach (var task in Flatten(folder.Folders)
+					.Where(t => !string.IsNullOrEmpty(t.OneNoteTaskID)))
+				{
+					yield return task;
+				}
+			}
+		}
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="task"></param>
+		/// <returns></returns>
+		public bool SaveTask(OutlookTask task)
+		{
+			if (!(outlook.Session.GetItemFromID(task.EntryID) is TaskItem item))
+			{
+				return false;
+			}
+
+			try
+			{
+				SetUserProperty(item, "OneNoteTaskID", task.OneNoteTaskID);
+				SetUserProperty(item, "OneNoteURL", task.OneNoteURL);
+				SetUserProperty(item, "OneNotePageID", task.OneNotePageID);
+				SetUserProperty(item, "OneNoteObjectID", task.OneNoteObjectID);
+
+				item.Save();
+			}
+			finally
+			{
+				Marshal.ReleaseComObject(item);
+			}
+
+			return true;
+		}
+
+
+		private void SetUserProperty(TaskItem item, string name, string value)
+		{
+			if (string.IsNullOrEmpty(value))
+			{
+				// UserProperties is a base-1 index!
+				var index = 1;
+				while (index <= item.UserProperties.Count && item.UserProperties[index].Name != name)
+				{
+					index++;
+				}
+
+				if (index < item.UserProperties.Count)
+				{
+					item.UserProperties.Remove(index);
+				}
+			}
+			else
+			{
+				var prop = item.UserProperties.Find(name)
+					?? item.UserProperties.Add(name, OlUserPropertyType.olText);
+
+				prop.Value = value;
+			}
+		}
+	}
+}
