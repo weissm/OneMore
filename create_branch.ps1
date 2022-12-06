1707f063f84f232324eb177987bdf265241367d1
Matthias Weiss <matthias.h.weiss@arcor.de>
2022-12-06 08:25:12 +0100

    Revert "Update create_branch.ps1"
    This reverts commit a0ff48f437d48cc68f41acc77efdaf95eebab67f.

--- create_branch.ps1
+++ create_branch.ps1
@@ -1,10 +1,17 @@
-git checkout -b improveMD34 mymain;
-git push --set-upstream origin improveMD34;
+git checkout -b testbranch mymain;
+git push --set-upstream origin testbranch;
 git merge improveMarkdown;
+git push;
 git merge addPythonSupport;
+git push;
 git merge fixWhenAll;
+git push;
 git merge improveGerTranslation;
+git push;
 git merge improvePlugInMenu;
+git push;
 git merge misc;
+git push;
 git merge makepublic;
+git push
 
