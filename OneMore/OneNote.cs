040c6c6ebb8ac71a9db7e856894e75179beba424
Matthias Weiss <matthias.h.weiss@arcor.de>
2022-12-06 16:47:32 +0100

    Update Page.cs

--- OneMore/OneNote.cs
+++ OneMore/OneNote.cs
@@ -35,8 +35,8 @@ namespace River.OneMoreAddIn
 	/// Wraps the OneNote interop API
 	/// </summary>
 	/// <see cref="https://docs.microsoft.com/en-us/office/client-developer/onenote/application-interface-onenote"/>
-	internal class OneNote : IAsyncDisposable, IDisposable
-	{
+	public class OneNote : IAsyncDisposable
+    {
 		public enum ExportFormat
 		{
 			EMF = PublishFormat.pfEMF,
