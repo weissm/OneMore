57e13dd2959e0476a84ba8e68bd991ff0a689358
Matthias Weiss <matthias.h.weiss@arcor.de>
2023-09-16 19:45:13 +0200

    Update MarkdownWriter.cs
    Merged w/ main manually
    
    Update Page.cs
    
    removed spaces
    
    Update Page.cs
    
    removed public statements here
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 1e2224af0b23cef344932180c7427075ad02a99c.
    
    intermediate quickfix
    
    Revert "intermediate quickfix"
    
    This reverts commit 35246a6da99919e4733851fabffa76582ba0b9bf.
    
    added await to onenote
    
    Update Page.cs
    
    removed spaces
    
    Update Page.cs
    
    removed public statements here
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 1e2224af0b23cef344932180c7427075ad02a99c.
    
    intermediate quickfix
    
    Revert "intermediate quickfix"
    
    This reverts commit 35246a6da99919e4733851fabffa76582ba0b9bf.
    
    added await to onenote
    
    Update Page.cs
    
    removed spaces
    
    Update Page.cs
    
    removed public statements here
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 1e2224af0b23cef344932180c7427075ad02a99c.
    
    intermediate quickfix
    
    Revert "intermediate quickfix"
    
    This reverts commit 35246a6da99919e4733851fabffa76582ba0b9bf.
    
    added await to onenote
    
    removed duplications

--- OneMore/Commands/File/Markdown/MarkdownWriter.cs
+++ OneMore/Commands/File/Markdown/MarkdownWriter.cs
@@ -10,6 +10,7 @@ namespace River.OneMoreAddIn.Commands
 	using River.OneMoreAddIn.Styles;
 	using System;
     using System.Collections.Generic;
+    using System.Collections.Generic;
 	using System.Drawing;
 	using System.Drawing.Imaging;
 	using System.IO;
@@ -108,7 +109,11 @@ namespace River.OneMoreAddIn.Commands
                 using var reader = new StreamReader(stream);
 
                 var clippy = new ClipboardProvider();
-                await clippy.SetText(reader.ReadToEnd());
+				var success = await clippy.SetText(reader.ReadToEnd(), true);
+				if (!success)
+				{
+					UIHelper.ShowInfo(Resx.Clipboard_locked);
+				}
             }
         }
 
@@ -121,6 +126,9 @@ namespace River.OneMoreAddIn.Commands
         {
 #if !LOG
             path = Path.GetDirectoryName(filename);
+			attachmentFolder = Path.GetFileNameWithoutExtension(filename);
+			attachmentPath = Path.Combine(path, attachmentFolder);
+
             using (writer = File.CreateText(filename))
 #endif
             {
@@ -131,12 +139,12 @@ namespace River.OneMoreAddIn.Commands
                     .Elements()
                     .ForEach(e => { PrefixClass prefix = new PrefixClass(); Write(e, ref prefix); });
 
-                // page level Images outside of any Outline
-                page.Root.Elements(ns + "Image")
-                    .ForEach(e => {
-                        PrefixClass prefix = new PrefixClass(); Write(e, ref prefix);
-                        writer.WriteLine();
-                    });
+				// page level Images outside of any Outline
+				page.Root.Elements(ns + "Image")
+					.ForEach(e => {
+						Write(e);
+						writer.WriteLine();
+					});
 
                 writer.WriteLine();
             }
@@ -330,7 +338,9 @@ namespace River.OneMoreAddIn.Commands
 					//case "p": logger.Write(Environment.NewLine); break;
 			}
 			writer.Write(prefix.indent + prefix.bullets + styleprefix + prefix.tags);
+			writer.Write(prefix.indent + prefix.bullets + styleprefix + prefix.tags);
 		}
+
 
 		private string WriteTag(XElement element, bool contained)
 		{
@@ -383,12 +393,14 @@ namespace River.OneMoreAddIn.Commands
 
 
 		private void WriteText(XCData cdata, bool startParagraph, bool contained)
+		private void WriteText(XCData cdata, bool startParagraph, bool contained)
 		{
+			// avoid overwriting input and creating side effects, e.g. when reusing page var
 			// avoid overwriting input and creating side effects, e.g. when reusing page var
 			cdata.Value = cdata.Value
 				.Replace("<br>", "") // usually followed by NL so leave it there
-//				.Replace("<br>", "  ") // usually followed by NL so leave it there
-//				.Replace("[", @"\[")   // escape to prevent confusion with md links
+				\\ .Replace("<br>", "  ") // usually followed by NL so leave it there
+				\\ .Replace("[", "\\[")   // escape to prevent confusion with md links
 				.TrimEnd();
 
 			var wrapper = cdata.GetWrapper();
