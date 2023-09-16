4b6ae616982e38179cde72f4c62a0d8a4d25b0db
Matthias Weiss <matthias.h.weiss@arcor.de>
2023-09-16 19:45:13 +0200

    Update MarkdownWriter.cs
    Merged w/ main manually
    
    Update Page.cs
    
    removed spaces
    
    Update Page.cs
    
    removed public statements here
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 1e2224af0b23cef344932180c7427075ad02a99c.
    
    intermediate quickfix
    
    Revert "intermediate quickfix"
    
    This reverts commit 35246a6da99919e4733851fabffa76582ba0b9bf.
    
    added await to onenote
    
    Update Page.cs
    
    removed spaces
    
    Update Page.cs
    
    removed public statements here
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 1e2224af0b23cef344932180c7427075ad02a99c.
    
    intermediate quickfix
    
    Revert "intermediate quickfix"
    
    This reverts commit 35246a6da99919e4733851fabffa76582ba0b9bf.
    
    added await to onenote
    
    Update Page.cs
    
    removed spaces
    
    Update Page.cs
    
    removed public statements here
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 1e2224af0b23cef344932180c7427075ad02a99c.
    
    intermediate quickfix
    
    Revert "intermediate quickfix"
    
    This reverts commit 35246a6da99919e4733851fabffa76582ba0b9bf.
    
    added await to onenote
    
    removed duplications

--- OneMore/Commands/File/Markdown/MarkdownWriter.cs
+++ OneMore/Commands/File/Markdown/MarkdownWriter.cs
@@ -11,7 +11,6 @@ namespace River.OneMoreAddIn.Commands
 	using River.OneMoreAddIn.UI;
 	using System;
     using System.Collections.Generic;
-    using System.Collections.Generic;
 	using System.Drawing;
 	using System.Drawing.Imaging;
 	using System.IO;
@@ -392,7 +391,6 @@ namespace River.OneMoreAddIn.Commands
 				case "code": styleprefix = ("`"); break;
 					//case "p": logger.Write(Environment.NewLine); break;
 			}
-			writer.Write(prefix.indent + prefix.bullets + styleprefix + prefix.tags);
 			writer.Write(prefix.indent + prefix.bullets + styleprefix + prefix.tags);
 		}
 
