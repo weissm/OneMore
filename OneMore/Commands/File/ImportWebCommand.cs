af6bb6eb17a8759776692175e429993367af5a32
Matthias Weiss <matthias.h.weiss@arcor.de>
2023-09-16 19:45:13 +0200

    Update Page.cs
    removed spaces
    
    Update Page.cs
    
    removed public statements here
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 1e2224af0b23cef344932180c7427075ad02a99c.

--- OneMore/Commands/File/ImportWebCommand.cs
+++ OneMore/Commands/File/ImportWebCommand.cs
@@ -21,6 +21,7 @@ namespace River.OneMoreAddIn.Commands
 	using Hap = HtmlAgilityPack;
 	using System.Text;
 	using System.Web.Script.Serialization;
+	using Win = System.Windows;
 	using Newtonsoft.Json.Linq;
 	using static River.OneMoreAddIn.Models.Page;
 	using Resx = Properties.Resources;
@@ -499,7 +500,7 @@ namespace River.OneMoreAddIn.Commands
 			var escapeID = "[OM-";
 
 			//,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-			await using (var one = new River.OneMoreAddIn.OneNote())
+			using (var one = new River.OneMoreAddIn.OneNote())
 			{
 				var page = await one.GetPage();
 				var ns = page.Namespace;
@@ -637,7 +638,7 @@ namespace River.OneMoreAddIn.Commands
 
         public async Task ImportMarkdownPostprocessing(string escapeID)
 		{
-			await using (var one = new River.OneMoreAddIn.OneNote())
+			using (var one = new River.OneMoreAddIn.OneNote())
 			{
 				var page = await one.GetPage();
 				var ns = page.Namespace;
