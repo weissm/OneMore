9ee259a8df78ab8096a1465647af8d7efd7bd4a1
Matthias Weiss <matthias.h.weiss@arcor.de>
2023-09-16 19:45:13 +0200

    Update Page.cs
    removed spaces
    
    Update Page.cs
    
    removed public statements here
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Added MarkDown features
    
    Update MarkdownWriter.cs
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Added MarkDown features"
    
    This reverts commit 4991d2dff979e4fce1e58e1a0bbca13ef0f0ee5d.
    
    Revert "Revert "Added MarkDown features""
    
    This reverts commit 64c6a95f37be83274f2cc50e4c921738ff1d97d9.
    
    Revert "Added MarkDown features"
    
    This reverts commit 1e2224af0b23cef344932180c7427075ad02a99c.

--- OneMore/Commands/File/Markdown/MarkdownWriter.cs
+++ OneMore/Commands/File/Markdown/MarkdownWriter.cs
@@ -109,11 +109,7 @@ namespace River.OneMoreAddIn.Commands
                 using var reader = new StreamReader(stream);
 
                 var clippy = new ClipboardProvider();
-				var success = await clippy.SetText(reader.ReadToEnd(), true);
-				if (!success)
-				{
-					MoreMessageBox.ShowError(null, Resx.Clipboard_locked);
-				}
+                await clippy.SetText(reader.ReadToEnd());
             }
         }
 
@@ -126,9 +122,6 @@ namespace River.OneMoreAddIn.Commands
         {
 #if !LOG
             path = Path.GetDirectoryName(filename);
-			attachmentFolder = Path.GetFileNameWithoutExtension(filename);
-			attachmentPath = Path.Combine(path, attachmentFolder);
-
             using (writer = File.CreateText(filename))
 #endif
             {
@@ -139,7 +132,7 @@ namespace River.OneMoreAddIn.Commands
                     .Elements()
                     .ForEach(e => { PrefixClass prefix = new PrefixClass(); Write(e, ref prefix); });
 
-				// page level Images outside of any Outline
+                // page level Images outside of any Outline
                 page.Root.Elements(ns + "Image")
                     .ForEach(e => {
                         PrefixClass prefix = new PrefixClass(); Write(e, ref prefix);
@@ -188,10 +181,10 @@ namespace River.OneMoreAddIn.Commands
 			bool startpara = false,
 			bool contained = false)
 		{
+
 			bool pushed = false;
 			bool dive = true;
 			var keepindents = prefix.indent;
-
 			switch (element.Name.LocalName)
 			{
 				case "OEChildren":
@@ -222,7 +215,7 @@ namespace River.OneMoreAddIn.Commands
 						break;
                     }
 					pushed = DetectQuickStyle(element);
-					Stylize(prefix);
+					Stylize(prefix); 
 					prefix.tags = ""; 
 					prefix.bullets = "";
 					WriteText(element.GetCData(), startpara, contained);
@@ -305,7 +298,7 @@ namespace River.OneMoreAddIn.Commands
 				if (quick != null)
 				{
 					// cite becomes italic
-					if (quick.Name == "cite") context.Enclosure = "*";
+					if (quick.Name == "cite") context.Enclosure = "_";
 					else if (quick.Name == "code") context.Enclosure = "`";
 				}
 				contexts.Push(context);
@@ -340,7 +333,6 @@ namespace River.OneMoreAddIn.Commands
 			writer.Write(prefix.indent + prefix.bullets + styleprefix + prefix.tags);
 		}
 
-
 		private string WriteTag(XElement element, bool contained)
 		{
 			var symbol = page.Root.Elements(ns + "TagDef")
@@ -348,21 +340,20 @@ namespace River.OneMoreAddIn.Commands
 				.Select(e => int.Parse(e.Attribute("symbol").Value))
 				.FirstOrDefault();
 			var retValue = "";
-			var retValue = "";
 
 			switch (symbol)
 			{
 				case 3:     // to do
 				case 8:     // client request
-				case 12:    // schedule/callback
-				case 28:    // todo prio 1
+				case 12:	// schedule/callback
+				case 28:	// todo prio 1
 				case 71:    // todo prio 2
 				case 94:    // discuss person a/b
 				case 95:    // discuss manager
 					var check = element.Attribute("completed").Value == "true" ? "x" : " ";
 					retValue = contained
 					  ? @"<input type=""checkbox"" disabled " + (check == "x" ? "checked" : "unchecked") + @" />"
-					  : ($"[{check}] ");
+					  : ($"- [{check}] ");
 
 					break;
 
@@ -386,7 +377,7 @@ namespace River.OneMoreAddIn.Commands
 				case 133: retValue = (":movie_camera: "); break;   // movie to see
 				case 132: retValue = (":book: "); break;           // book to read
 				case 140: retValue = (":zap: "); break;            // lightning bolt																	
-				default: retValue = (":o: "); break;									   // retValue = (":o: "); break;
+				default: break;									   // retValue = (":o: "); break;
 			}
 			return retValue;
 		}
@@ -397,8 +388,8 @@ namespace River.OneMoreAddIn.Commands
 			// avoid overwriting input and creating side effects, e.g. when reusing page var
 			cdata.Value = cdata.Value
 				.Replace("<br>", "") // usually followed by NL so leave it there
-				// .Replace("<br>", "  ") // usually followed by NL so leave it there
-				// .Replace("[", "\\[")   // escape to prevent confusion with md links
+//				.Replace("<br>", "  ") // usually followed by NL so leave it there
+//				.Replace("[", @"\[")   // escape to prevent confusion with md links
 				.TrimEnd();
 
 			var wrapper = cdata.GetWrapper();
@@ -467,7 +458,7 @@ namespace River.OneMoreAddIn.Commands
 			using var stream = new MemoryStream(binhex, 0, binhex.Length);
 			using var image = Image.FromStream(stream);
 
-			var prefix = PathHelper.CleanFileName(page.Title).Replace(" ", string.Empty);
+			var prefix = page.Title.Replace(" ", string.Empty);
 			var name = $"{prefix}_{++imageCounter}.png";
 			var filename = Path.Combine(attachmentPath, name);
 #if !LOG
@@ -544,12 +535,27 @@ namespace River.OneMoreAddIn.Commands
 
 			// table needs a blank line before it
 			writer.WriteLine();
-			bool first_row = true;
+
+			// header
+			writer.Write(indents + "|");
+			for (int i = 0; i < table.ColumnCount; i++)
+			{
+				writer.Write($" {TableCell.IndexToLetters(i + 1)} |");
+			}
+			writer.WriteLine();
+
+			// separator
+			writer.Write("|");
+			for (int i = 0; i < table.ColumnCount; i++)
+			{
+				writer.Write(" :--- |");
+			}
+			writer.WriteLine();
 
 			// data
 			foreach (var row in table.Rows)
 			{
-				writer.Write(indents + "| ");
+				writer.Write("| ");
 				foreach (var cell in row.Cells)
 				{
 					cell.Root
@@ -560,19 +566,7 @@ namespace River.OneMoreAddIn.Commands
 					writer.Write(" | ");
 				}
 				writer.WriteLine();
-				if (first_row)
-				{
-					first_row = false;
-                    // separator
-                    writer.Write(indents + "|");
-                    for (int i = 0; i < table.ColumnCount; i++)
-                    {
-                        writer.Write(" :--- |");
-                    }
-                    writer.WriteLine();
-                }
-
-            }
-        }
+			}
+		}
 	}
 }
