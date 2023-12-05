2f8bc1a979ebd593bb521394c240f2260586899e
Matthias Weiss <matthias.h.weiss@arcor.de>
2023-12-05 09:48:14 +0100

    Added GetoptsStyle to have direct access to PlugIn Menu
    Added GetoptsStyle to have direct access to PlugIn Menu
    
    merged getoptsStyle
    
    Update Page.cs
    
    removed system data update
    
    Manual merge of page dialog incl. getopts
    
    reworked changes
    
    removed duplication
    
    revwert changes

--- OneMore/Commands/Settings/PluginsSheet.cs
+++ OneMore/Commands/Settings/PluginsSheet.cs
@@ -225,6 +225,23 @@ namespace River.OneMoreAddIn.Settings
 			}
 		}
 
+
+		private async void RunItem(object sender, EventArgs e)
+		{
+			if (gridView.SelectedCells.Count == 0)
+				return;
+
+			int rowIndex = gridView.SelectedCells[0].RowIndex;
+			if (rowIndex >= plugins.Count)
+				return;
+
+			var plugin = plugins[rowIndex];
+		
+			var runPluginCommand = new RunPluginCommand();
+			runPluginCommand.SetLogger(Logger.Current);
+			await runPluginCommand.Execute(plugin.Path);
+		}
+
 		public override bool CollectSettings()
 		{
 			if (updated)
@@ -245,21 +262,8 @@ namespace River.OneMoreAddIn.Settings
 
 		}
 
-		private async void runLabel_Click(object sender, EventArgs e)
-        {
-            if (gridView.SelectedCells.Count == 0)
-                return;
-
-            int rowIndex = gridView.SelectedCells[0].RowIndex;
-            if (rowIndex >= plugins.Count)
-                return;
-
-            var plugin = plugins[rowIndex];
-
-            var runPluginCommand = new RunPluginCommand();
-            runPluginCommand.SetLogger(Logger.Current);
-            await runPluginCommand.Execute(plugin.Path);
-        }
-
-    }
+		private void runLabel_Click(object sender, EventArgs e)
+		{
+		}
+	}
 }
