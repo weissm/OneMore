6924bcd2be43b0b9152234fa6c668291d49e0ac9
Matthias Weiss <matthias.h.weiss@arcor.de>
2024-03-24 13:45:14 +0100

    added await to onenote

--- OneMore/Commands/File/ImportWebCommand.cs
+++ OneMore/Commands/File/ImportWebCommand.cs
@@ -476,7 +476,7 @@ namespace River.OneMoreAddIn.Commands
 			var escapeID = "[OM-";
 
 			//,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-			using (var one = new River.OneMoreAddIn.OneNote())
+			await using (var one = new River.OneMoreAddIn.OneNote())
 			{
 				var page = await one.GetPage();
 				var ns = page.Namespace;
@@ -614,7 +614,7 @@ namespace River.OneMoreAddIn.Commands
 
         public async Task ImportMarkdownPostprocessing(string escapeID)
 		{
-			using (var one = new River.OneMoreAddIn.OneNote())
+			await using (var one = new River.OneMoreAddIn.OneNote())
 			{
 				var page = await one.GetPage();
 				var ns = page.Namespace;
