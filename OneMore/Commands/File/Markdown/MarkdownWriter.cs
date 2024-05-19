66c51e7e2adaf0bfac8d1e9001503556193bd5d9
Matthias Weiss <matthias.h.weiss@arcor.de>
2024-05-19 14:40:47 +0200

    manual merge

--- OneMore/Commands/File/Markdown/MarkdownWriter.cs
+++ OneMore/Commands/File/Markdown/MarkdownWriter.cs
@@ -12,7 +12,6 @@ namespace River.OneMoreAddIn.Commands
 	using River.OneMoreAddIn.UI;
 	using System;
     using System.Collections.Generic;
-    using System.Collections.Generic;
 	using System.Drawing;
 	using System.Drawing.Imaging;
 	using System.IO;
@@ -218,12 +217,9 @@ namespace River.OneMoreAddIn.Commands
 			bool startpara = false,
 			bool contained = false)
 		{
-
 			bool pushed = false;
 			bool dive = true;
-			var keepindents = prefix.indent;
 
-			var keepindents = prefix.indent;
 			switch (element.Name.LocalName)
 			{
 				case "OEChildren":
@@ -273,7 +269,6 @@ namespace River.OneMoreAddIn.Commands
 
 				case "Tag":
 					prefix.tags += WriteTag(element, contained);
-					prefix.tags += WriteTag(element, contained);
 					break;
 
 				case "T":
@@ -282,7 +277,7 @@ namespace River.OneMoreAddIn.Commands
 						break;
                     }
 					pushed = DetectQuickStyle(element);
-					Stylize(prefix); 
+					if (startpara) Stylize(prefix);
 					prefix.tags = ""; 
 					prefix.bullets = "";
 					WriteText(element.GetCData(), startpara, contained);
@@ -349,15 +344,8 @@ namespace River.OneMoreAddIn.Commands
 
 				// if not in a table cell
 				// or in a cell and this OE is followed by another OE
-				if (!contained && (element.NextNode != null))
-				if (!contained && (element.NextNode != null))
+				if (!contained || (element.NextNode != null))
 				{
-					writer.WriteLine("");
-				} else if (contained)
-                {
-					writer.Write("<br>");
-                }
-				prefix.indent = keepindents;
 					writer.WriteLine("");
 				} else if (contained)
                 {
@@ -383,7 +371,7 @@ namespace River.OneMoreAddIn.Commands
 					var name = quick.Name.ToLower();
 
 					// cite becomes italic
-					if (quick.Name == "cite") context.Enclosure = "_";
+					if (quick.Name == "cite") context.Enclosure = "*";
 					else if (quick.Name == "code") context.Enclosure = "`";
 				}
 
@@ -417,7 +405,6 @@ namespace River.OneMoreAddIn.Commands
 					//case "p": logger.Write(Environment.NewLine); break;
 			}
 			writer.Write(prefix.indent + prefix.bullets + styleprefix + prefix.tags);
-			writer.Write(prefix.indent + prefix.bullets + styleprefix + prefix.tags);
 		}
 
 
@@ -433,8 +420,8 @@ namespace River.OneMoreAddIn.Commands
 			{
 				case 3:     // to do
 				case 8:     // client request
-				case 12:	// schedule/callback
-				case 28:	// todo prio 1
+				case 12:    // schedule/callback
+				case 28:    // todo prio 1
 				case 71:    // todo prio 2
 				case 94:    // discuss person a/b
 				case 95:    // discuss manager
@@ -465,7 +452,7 @@ namespace River.OneMoreAddIn.Commands
 				case 133: retValue = (":movie_camera: "); break;   // movie to see
 				case 132: retValue = (":book: "); break;           // book to read
 				case 140: retValue = (":zap: "); break;            // lightning bolt																	
-				default: break;									   // retValue = (":o: "); break;
+				default: retValue = (":o: "); break;									   // retValue = (":o: "); break;
 			}
 			return retValue;
 		}
@@ -473,7 +460,6 @@ namespace River.OneMoreAddIn.Commands
 
 		private void WriteText(XCData cdata, bool startParagraph, bool contained)
 		{
-			// avoid overwriting input and creating side effects, e.g. when reusing page var
 			// avoid overwriting input and creating side effects, e.g. when reusing page var
 			cdata.Value = cdata.Value
 				.Replace("<br>", "") // usually followed by NL so leave it there
