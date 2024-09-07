9e669bb3a4ac6fd2d5018e1d778d47356fa57e78
Matthias Weiss <matthias.h.weiss@arcor.de>
2024-09-07 15:20:48 +0200

    Revert "Update Page.cs"
    This reverts commit 341453276eab1f0d4b21ff2fce6304eae21bbcde.
    
    fixed merge issue
    
    Revert "manual merge"
    
    This reverts commit 67a16b1b148c4f096bd6b0e32b420b07858a85ed.
    
    manual merged
    
    manual merge
    
    added public
    
    changed Win32WindowHandle to public
    
    Made OneNote public
    
    This reverts commit 3042e9038d50f95475f58ca0b6b3171cb1b405dd.
    
    Revert "manual merge"
    
    This reverts commit 67a16b1b148c4f096bd6b0e32b420b07858a85ed.
    
    manual merged
    
    manual merge
    
    Update Page.cs
    
    Add using System.Threading.Tasks;
    
    manual merge
    
    manual merged
    
    Add using System.Threading.Tasks;
    
    Update Page.cs
    
    manual merge
    
    manual merged
    
    added public statement
    
    Add using System.Threading.Tasks;
    
    Update Page.cs
    
    Made OneNote public
    
    This reverts commit 3042e9038d50f95475f58ca0b6b3171cb1b405dd.
    
    Update Page.cs
    
    Add using System.Threading.Tasks;
    
    manual merge
    
    manual merged
    
    Add using System.Threading.Tasks;
    
    Update Page.cs
    
    added public

--- OneMore/Models/Page.cs
+++ OneMore/Models/Page.cs
@@ -22,7 +22,7 @@ namespace River.OneMoreAddIn.Models
 	/// <summary>
 	/// Wraps a page with helper methods
 	/// </summary>
-	internal partial class Page
+	public partial class Page
 	{
 		public int TopOutlinePosition = 86;
 
@@ -138,8 +138,7 @@ namespace River.OneMoreAddIn.Models
 		/// <summary>
 		/// Gets the root element of the page
 		/// </summary>
-		public XElement Root { get; private set; }
-
+		public XElement Root { get; set; }
 
 		// TODO: this is inconsistent! It gets the plain text but allows setting complex CDATA
 		public string Title
