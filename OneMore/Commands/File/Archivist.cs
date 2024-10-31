289dd7b65a1083d47036daa90d08085ef68bc8a8
Matthias Weiss <matthias.h.weiss@arcor.de>
2024-10-31 12:54:38 +0100

    squased Markdown commits
    squased Markdown commits
    
    Revert "squased Markdown commits"
    
    This reverts commit 615c88d6ffb15b2ad5c2a1dfdd79c9eab7548573.
    
    removed spaces
    
    Update InsertBoxCommand.cs
    
    merged input
    
    removed debug
    
    added handling of bullets accross paragraphs and cleaned up parameter handling
    
    Changed filepath handing to allow for spaces in file names
    
    Merge branch 'improveMarkdownPullRequest' of github.com:weissm/OneMore into improveMarkdownPullRequest
    
    chnaged markdown tag and table handling
    
    Worked in review comments
    
    squased Markdown commits
    
    squased Markdown commits
    
    Revert "squased Markdown commits"
    
    This reverts commit 615c88d6ffb15b2ad5c2a1dfdd79c9eab7548573.
    
    removed spaces
    
    Update InsertBoxCommand.cs
    
    merged input
    
    squased Markdown commits
    
    squased Markdown commits
    
    Revert "squased Markdown commits"
    
    This reverts commit 615c88d6ffb15b2ad5c2a1dfdd79c9eab7548573.
    
    removed spaces
    
    Update InsertBoxCommand.cs
    
    merged input
    
    Revert "Merge branch 'improveMarkdown' of github.com:weissm/OneMore into improveMarkdown"
    
    This reverts commit c296f72596dce8b386a6e80806e5f705bacbfe1d.
    
    Merge branch 'improveMarkdown' of github.com:weissm/OneMore into improveMarkdown
    
    Revert "squased Markdown commits"
    
    This reverts commit 93411d9942eebc0ea384015262521ce583b0edc6.
    
    Revert "Added nested table example"
    
    This reverts commit d5d0122b3c33fbf472681b55450392d1e0d0760e.
    
    Added nested table example
    
    Added nested table example
    
    Worked in review comments
    
    added comments
    
    squased Markdown commits
    
    squased Markdown commits
    
    Revert "squased Markdown commits"
    
    This reverts commit 615c88d6ffb15b2ad5c2a1dfdd79c9eab7548573.
    
    removed spaces
    
    Update InsertBoxCommand.cs
    
    merged input
    
    squased Markdown commits
    
    squased Markdown commits
    
    Revert "squased Markdown commits"
    
    This reverts commit 615c88d6ffb15b2ad5c2a1dfdd79c9eab7548573.
    
    removed spaces
    
    Update InsertBoxCommand.cs
    
    merged input
    
    Revert "Merge branch 'improveMarkdown' of github.com:weissm/OneMore into improveMarkdown"
    
    This reverts commit c296f72596dce8b386a6e80806e5f705bacbfe1d.
    
    Merge branch 'improveMarkdown' of github.com:weissm/OneMore into improveMarkdown
    
    Revert "squased Markdown commits"
    
    This reverts commit 93411d9942eebc0ea384015262521ce583b0edc6.
    
    Revert "Added nested table example"
    
    This reverts commit d5d0122b3c33fbf472681b55450392d1e0d0760e.
    
    Added nested table example
    
    Added nested table example

--- OneMore/Commands/File/Archivist.cs
+++ OneMore/Commands/File/Archivist.cs
@@ -367,6 +367,25 @@ namespace River.OneMoreAddIn.Commands
 		/// </summary>
 		/// <param name="root"></param>
 		/// <param name="filename"></param>
+        public async Task<string> ExportOnenote2Markdown(string title)
+		{
+            string targetDir = "c:\\tmp\\";
+            await using (var one = new River.OneMoreAddIn.OneNote())
+            {
+                Page page;
+                page = await one.GetPage();
+
+                var archivist = new Archivist(one);
+                var pageTitleFull = page.Title;
+                var pageTitle = page.Title.Replace(" ", string.Empty);
+                string fullFileName = targetDir + pageTitle + ".md";
+
+                archivist.ExportMarkdown(page, filename: fullFileName, withAttachments: true);
+                var _outputData = File.ReadAllText(fullFileName);
+//                Console.WriteLine(_outputData);
+				return _outputData;
+            }
+        }
 		public void ExportMarkdown(Page page, string filename, bool withAttachments)
 		{
 			try
